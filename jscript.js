fae1a0cd8915cfcc2d8051a69bfecf939aafc34e
Rahul Gupta <useritch96@gmail.com>
2016-09-01 21:08:50 +0530

    Fixed the site

--- jscript.js
+++ jscript.js
@@ -1,21 +1,24 @@
 var main = function() {
+	var icon = document.getElementsByClassName('search_icon');
+	var iconWidth = icon[0].clientWidth;
 	$('.search_icon').on('click', function(){
 		$(this).animate({width: '0px'}, 400).fadeOut(0);
-		$('.search_box').animate({width: '50vw'}, 400).css({display: 'block'}).focus();
+		$('.search_box').animate({width: '+=60vw'}, 400).css({display: 'block'}).focus();
 	});
 	$('.search_icon').hover(
 		function() {
-			$(this).animate({width: '+=10px'}, 400);
+			$(this).animate({width: '+=10pt'}, 400);
 		},
 		function(){
-			$(this).animate({width: '-=10px'}, 400);
+			$(this).animate({width: '-=10pt'}, 400);
 		}
 	);
 	$(document).keyup(function(e){
 		//pressing the escape key
 		if(e.keyCode == 27 ) {
 			$('.search_box').animate({width: '0px'}, 400).fadeOut(0).blur();
-			$('.search_icon').animate({width: '5%'}, 400).fadeIn(0);
+			console.log(iconWidth);
+			$('.search_icon').animate({width: iconWidth}, 400).fadeIn(0);
 		}
 		// pressing the enter key
 		if(e.keyCode == 13) {
@@ -34,7 +37,7 @@ var main = function() {
 				success: function(data, status, jqXHR) {
 					$('.result').html('');
 					for(var i = 0; i < data[1].length; i ++) {
-						$('.result').append("<div class='container'><div class='well'><a target='blank' href="+data[3][i]+"><h2>"+data[1][i]+"</h2><p>"+data[2][i]+"</p></a></div></div>");
+						$('.result').append("<div class='container'><div class='well'><a target='blank' href="+data[3][i]+"><h2 id='heading'>"+data[1][i]+"</h2><p id='description'>"+data[2][i]+"</p></a></div></div>");
 					}
 				}
 			});
